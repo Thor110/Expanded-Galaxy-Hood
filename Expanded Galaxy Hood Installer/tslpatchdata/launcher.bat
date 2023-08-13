6595688757b8be9c7cfe56732bbca7570197aab3
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2023-08-13 03:42:16 +0100

    Launcher Updated

--- Expanded Galaxy Hood Installer/tslpatchdata/launcher.bat
+++ Expanded Galaxy Hood Installer/tslpatchdata/launcher.bat
@@ -79,7 +79,6 @@ ren "Override\k_pkor_33arenter.ncs.port" "k_pkor_33arenter.ncs"
 ren "Override\k_ptat17af_enter.ncs.port" "k_ptat17af_enter.ncs"
 ren "Override\k_ptat17_enter.ncs.port" "k_ptat17_enter.ncs"
 ren "Override\k_ptat18ac_enter.ncs.port" "k_ptat18ac_enter.ncs"
-ren Movies\ObsidianEnt.bik ObsidianEnt.bik.port
 goto :KOTOR2-C
 :ENABLE
 ren "Movies\ObsidianEnt.bik" "ObsidianEnt.bik.port"
