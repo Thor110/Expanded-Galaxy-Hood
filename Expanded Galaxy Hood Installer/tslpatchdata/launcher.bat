f0e40ab7639f39556d8d0dc3701235c19ce3cc4f
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2024-04-27 08:13:56 +0100

    launcher update

--- Expanded Galaxy Hood Installer/tslpatchdata/launcher.bat
+++ Expanded Galaxy Hood Installer/tslpatchdata/launcher.bat
@@ -59,12 +59,14 @@ ren dialog.tlk dialog.tlk.main
 ren lips\001EBO_loc.mod 001EBO_loc.mod.main
 ren Modules\001ebo.mod 001ebo.mod.main
 ren StreamMusic\mus_sion.wav mus_sion.wav.main
+ren swkotor2.exe swkotor2.exe.main
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.main"
 ren Movies\ObsidianEnt.bik.port ObsidianEnt.bik
 ren dialog.tlk.port dialog.tlk
 ren lips\001EBO_loc.mod.port 001EBO_loc.mod
 ren Modules\001ebo.mod.port 001ebo.mod
 ren StreamMusic\mus_sion.wav.port mus_sion.wav
+ren swkotor2.exe.port swkotor2.exe
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.port" "%%i"
 echo KotOR1 Enabled!
 goto :ENABLE
@@ -112,12 +114,14 @@ ren dialog.tlk dialog.tlk.main
 ren lips\001EBO_loc.mod 001EBO_loc.mod.main
 ren Modules\001ebo.mod 001ebo.mod.main
 ren StreamMusic\mus_sion.wav mus_sion.wav.main
+ren swkotor2.exe swkotor2.exe.main
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.main"
 ren Movies\ObsidianEnt.bik.port ObsidianEnt.bik
 ren dialog.tlk.port dialog.tlk
 ren lips\001EBO_loc.mod.port 001EBO_loc.mod
 ren Modules\001ebo.mod.port 001ebo.mod
 ren StreamMusic\mus_sion.wav.port mus_sion.wav
+ren swkotor2.exe.port swkotor2.exe
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.port" "%%i"
 echo KotOR1 Enabled!
 goto :STEAMORDISC
@@ -127,12 +131,14 @@ ren dialog.tlk dialog.tlk.port
 ren lips\001EBO_loc.mod 001EBO_loc.mod.port
 ren Modules\001ebo.mod 001ebo.mod.port
 ren StreamMusic\mus_sion.wav mus_sion.wav.port
+ren swkotor2.exe swkotor2.exe.port
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i" "%%i.port"
 ren Movies\ObsidianEnt.bik.main ObsidianEnt.bik
 ren dialog.tlk.main dialog.tlk
 ren lips\001EBO_loc.mod.main 001EBO_loc.mod
 ren Modules\001ebo.mod.main 001ebo.mod
 ren StreamMusic\mus_sion.wav.main mus_sion.wav
+ren swkotor2.exe.main swkotor2.exe
 for /f "delims=|" %%i in (port-file-list.txt) do ren "Override\%%i.main" "%%i"
 echo KotOR2 Enabled!
 :STEAMORDISC
