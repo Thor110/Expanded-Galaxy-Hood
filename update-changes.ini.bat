c234a2995529a536dadb775df7cae2c852a52e8f
Edward James Gordon <invaderzimtwo@hotmail.co.uk>
2025-02-28 04:58:03 +0000

    Changes Updater Prep

--- update-changes.ini.bat
+++ update-changes.ini.bat
@@ -2,27 +2,22 @@
 rem Thor110
 rem generate override file list for changes.ini
 
-echo rewrite this specifically for brotherhood first!
-echo IE : rewrite these and make the changes source folder!
-echo update-scripts.bat
-echo update-changes.ini.bat
-echo update-everything.bat
-pause
-exit
-
 echo compiling all modules in all relevant directories
 echo run this third
 
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-switch.ini"
-del "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-amazon.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-android.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-disc.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-gog.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-legacypc.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-steam.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-switch.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-xbox.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-macdisc.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-macappstore.ini"
+del "Expanded Galaxy Hood Installer\tslpatchdata\changes-macsteam.ini"
 
-dir "Expanded Galaxy Main Installer\tslpatchdata\Override" /b > temp-list.txt
+dir "Expanded Galaxy Hood Installer\tslpatchdata\Override" /b > temp-list.txt
 
 echo Generating Override Install List entries for the changes.ini files...
 
@@ -43,14 +38,17 @@ echo Override Install List Generated!
 
 echo Creating changes.ini files...
 
-type changes\changes-amazon-intro.ini		changes\changes.ini changes\changes-amazon-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-amazon.ini"
-type changes\changes-android-intro.ini		changes\changes.ini changes\changes-android-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-android.ini"
-type changes\changes-disc-intro.ini			changes\changes.ini changes\changes-disc-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-disc.ini"
-type changes\changes-gog-intro.ini			changes\changes.ini changes\changes-gog-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-gog.ini"
-type changes\changes-legacypc-intro.ini		changes\changes.ini changes\changes-legacypc-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-legacypc.ini"
-type changes\changes-steam-intro.ini		changes\changes.ini changes\changes-steam-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-steam.ini"
-type changes\changes-switch-intro.ini		changes\changes.ini changes\changes-switch-outro.ini		override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-switch.ini"
-type changes\changes-xbox-intro.ini			changes\changes.ini changes\changes-xbox-outro.ini			override.txt > "Expanded Galaxy Main Installer\tslpatchdata\changes-xbox.ini"
+type changes\changes-amazon-intro.ini		changes\changes.ini changes\changes-amazon-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-amazon.ini"
+type changes\changes-android-intro.ini		changes\changes.ini changes\changes-android-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-android.ini"
+type changes\changes-disc-intro.ini			changes\changes.ini changes\changes-disc-outro.ini			override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-disc.ini"
+type changes\changes-gog-intro.ini			changes\changes.ini changes\changes-gog-outro.ini			override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-gog.ini"
+type changes\changes-legacypc-intro.ini		changes\changes.ini changes\changes-legacypc-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-legacypc.ini"
+type changes\changes-steam-intro.ini		changes\changes.ini changes\changes-steam-outro.ini			override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-steam.ini"
+type changes\changes-switch-intro.ini		changes\changes.ini changes\changes-switch-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-switch.ini"
+type changes\changes-xbox-intro.ini			changes\changes.ini changes\changes-xbox-outro.ini			override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-xbox.ini"
+type changes\changes-macdisc-intro.ini		changes\changes.ini changes\changes-macdisc-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-macdisc.ini"
+type changes\changes-macappstore-intro.ini	changes\changes.ini changes\changes-macappstore-outro.ini	override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-macappstore.ini"
+type changes\changes-macsteam-intro.ini		changes\changes.ini changes\changes-macsteam-outro.ini		override.txt > "Expanded Galaxy Hood Installer\tslpatchdata\changes-macsteam.ini"
 
 del override.txt
 
